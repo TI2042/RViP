49b33c24f7dfe3ddc324de2729a6db4c7ceb7ef0
TI2042 <tis3.1415@gmail.com>
2021-10-31 13:32:10 +0400

    Lamport

--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from multiprocessing import Process, Pipe
+from os import getpid
+from datetime import datetime
 
 
-# Press the green button in the gutter to run the script.
+def local_time(counter):
+    return ' (LAMPORT_TIME={}, LOCAL_TIME={})'.format(counter,
+                                                      datetime.now())
+
+
+def calc_recv_timestamp(recv_time_stamp, counter):
+    return max(recv_time_stamp, counter) + 1
+
+
+def event(pid, counter):
+    counter += 1
+    print('Something happened in {} !'. \
+          format(pid) + local_time(counter))
+    return counter
+
+
+def send_message(pipe, pid, counter):
+    counter += 1
+    pipe.send(('Empty shell', counter))
+    print('Message sent from ' + str(pid) + local_time(counter))
+    return counter
+
+
+def recv_message(pipe, pid, counter):
+    message, timestamp = pipe.recv()
+    counter = calc_recv_timestamp(timestamp, counter)
+    print('Message received at ' + str(pid) + local_time(counter))
+    return counter
+
+
+def process_one(pipe12):
+    pid = getpid()
+    counter = 0
+    counter = event(pid, counter)
+    counter = send_message(pipe12, pid, counter)
+    counter = event(pid, counter)
+    counter = recv_message(pipe12, pid, counter)
+    counter = event(pid, counter)
+
+
+def process_two(pipe21, pipe23):
+    pid = getpid()
+    counter = 0
+    counter = recv_message(pipe21, pid, counter)
+    counter = send_message(pipe21, pid, counter)
+    counter = send_message(pipe23, pid, counter)
+    counter = recv_message(pipe23, pid, counter)
+
+
+def process_three(pipe32):
+    pid = getpid()
+    counter = 0
+    counter = recv_message(pipe32, pid, counter)
+    counter = send_message(pipe32, pid, counter)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    oneandtwo, twoandone = Pipe()
+    twoandthree, threeandtwo = Pipe()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    process1 = Process(target=process_one,
+                       args=(oneandtwo,))
+    process2 = Process(target=process_two,
+                       args=(twoandone, twoandthree))
+    process3 = Process(target=process_three,
+                       args=(threeandtwo,))
+
+    process1.start()
+    process2.start()
+    process3.start()
+
+    process1.join()
+    process2.join()
+    process3.join()
